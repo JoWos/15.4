c2498f1f6063c1290ad8e0c4eb4be59e5c7ffafc
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-08-23 23:10:48 +0200

    Add phone features

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function Phone(brand, price, color) {
+function Phone(brand, price, color, system, camera, releaseDate, discontinue) {
     this.brand = brand;
     this.price = price;
     this.color = color;
@@ -11,13 +11,13 @@ function Phone(brand, price, color) {
 }
 
 Phone.prototype.printInfo = function () {
-    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' PLN.');
+    console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + ' PLN. It was released in ' + this.releaseDate + ' and discontinued in ' + this.discontinue + '. It runs ' + this.system + '. The camera has ' + this.camera + 'Mpx.');
 }
 
 
-var SamsungGalaxyS6 = new Phone('Samsung', 1500, 'Gold platinum');
-var iPhone6s = new Phone('Apple', 2000, 'silver');
-var OnePlusOne = new Phone('OnePlus', 800, 'black');
+var SamsungGalaxyS6 = new Phone('Samsung', 1500, 'Gold platinum', 'Android 5.0', 16, 2015, 2016);
+var iPhone6s = new Phone('Apple', 2000, 'silver', 'iOS 9', 12, 2015, 2016 );
+var OnePlusOne = new Phone('OnePlus', 800, 'black', 'Android 4.4', 13, 2014, 2016);
 
 SamsungGalaxyS6.printInfo();
 iPhone6s.printInfo();
